bf61a8b569cb5bc7a90426f4347aec40450f0e6f
Seehwan Yoo <seehwan.yoo@assam.dankook.ac.kr>
2016-10-20 10:46:12 +0900

    setitimer added
    set iterative timer
    
    fire timer for every 1 sec
    after 3 sec, exit

--- signal.c
+++ signal.c
@@ -3,8 +3,12 @@
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 void signal_handler(int signo);
+int count = 0;
+
 
 int main()
 {
@@ -14,6 +18,13 @@ int main()
 	new_sa.sa_handler = &signal_handler;
 	sigaction(SIGALRM, &new_sa, &old_sa);
 
+	struct itimerval new_itimer, old_itimer;
+	new_itimer.it_interval.tv_sec = 1;
+	new_itimer.it_interval.tv_usec = 0;
+	new_itimer.it_value.tv_sec = 1;
+	new_itimer.it_value.tv_usec = 0;
+	setitimer(ITIMER_REAL, &new_itimer, &old_itimer);
+
 	while (1);
 	return 0;
 }
@@ -21,5 +32,19 @@ int main()
 void signal_handler(int signo)
 {
 	printf("signaled! %d \n", signo);
+	count++;
+
+	if (count == 3) exit(0);
 }
+
+
+
+
+
+
+
+
+
+
+
 
